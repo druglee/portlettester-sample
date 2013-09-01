69429c511e01d571cfc4143c4a907c858fca2cbf
Druglee <druglee@163.com>
2013-09-01 20:17:52 +0800

    Added a few test cases

--- src/main/java/com/portletguru/portlettester/sample/SamplePortlet.java
+++ src/main/java/com/portletguru/portlettester/sample/SamplePortlet.java
@@ -3,12 +3,72 @@
  */
 package com.portletguru.portlettester.sample;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.GenericPortlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.ProcessAction;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * @author Derek Linde Li
- *
+ * 
  */
 public class SamplePortlet extends GenericPortlet {
+
+	public static final String INIT_PARAM_KEY = "initParamKey";
+	public static final String TEST_ACTION_NAME = "testAction";
+
+	public static final String PARAM_USER_ID = "paramUserId";
+	public static final String PREFS_PREFERRED_USER_ID = "preferredUserId";
 	
+	public static final String REQUEST_ATTR_SAVED_USER_ID = "savedUserId";
+	
+	private boolean isParamSet;
+
+	@Override
+	public void init(PortletConfig config) throws PortletException {
+		super.init(config);
+		String initParam = getInitParameter(INIT_PARAM_KEY);
+		isParamSet = "true".equals(initParam);
+	}
+	
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+		
+		if(!isParamSet) {
+			throw new PortletException("Portlet not correctly initialized.");
+		}
+		
+		String savedUserId = request.getParameter(PARAM_USER_ID);
+		if(StringUtils.isNotEmpty(savedUserId)) {
+			request.setAttribute(REQUEST_ATTR_SAVED_USER_ID, savedUserId);
+		}
+	}
+
+	@ProcessAction(name = TEST_ACTION_NAME)
+	public void processActionSaveUserId(ActionRequest request, ActionResponse response)
+			throws IOException, PortletException {
+		
+		if(!isParamSet) {
+			throw new PortletException("Portlet not correctly initialized.");
+		}
+		
+		String userId = request.getParameter(PARAM_USER_ID);
+		if(StringUtils.isNotEmpty(userId)) {
+			PortletPreferences preferences = request.getPreferences();
+			preferences.setValue(PREFS_PREFERRED_USER_ID, userId);
+			preferences.store();
+		}
+		
+		response.setRenderParameter(PARAM_USER_ID, userId);
+	}
 }
--- src/test/java/com/portletguru/portlettester/sample/SamplePortletTest.java
+++ src/test/java/com/portletguru/portlettester/sample/SamplePortletTest.java
@@ -3,16 +3,30 @@
  */
 package com.portletguru.portlettester.sample;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.portletguru.portlettester.PortletConfigGenerator;
 import com.portletguru.portlettester.PortletTester;
+import com.portletguru.portlettester.mocks.ActionRequestGenerator;
+import com.portletguru.portlettester.mocks.ActionResponseGenerator;
+import com.portletguru.portlettester.mocks.RenderRequestGenerator;
+import com.portletguru.portlettester.mocks.RenderResponseGenerator;
 
 /**
  * @author Derek Linde Li
@@ -31,12 +45,88 @@ public class SamplePortletTest {
 	@Before
 	public void setup() throws PortletException {
 		portlet = new SamplePortlet();
-		portletTester.initPortlet(portlet);
+		
+		PortletConfigGenerator configGenerator = portletTester.getPortletConfigGenerator();
+		configGenerator.addInitParameter(SamplePortlet.INIT_PARAM_KEY, "true");
+		portletTester.initPortlet(portlet, configGenerator.generatePortletConfig());
 	}
 	
+	
+	/**
+	 * Test doView() when portlet is not correctly initialized
+	 *  
+	 * @throws PortletException
+	 */
 	@Test
-	public void test() {
-		Assert.assertTrue(true);
+	public void testIncorrectPortletInitialization() throws PortletException {
+		portletTester.initPortlet(portlet);
+		RenderRequestGenerator requestGenerator = portletTester.getRenderRequestGenerator();
+		RenderResponseGenerator responseGenerator = portletTester.getRenderResponseGenerator();
+		
+		PortletException e = null;
+		try {
+			portlet.doView(requestGenerator.generateRequest(), responseGenerator.generateResponse());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (PortletException ex) {
+			e = ex;
+		}
+		assertNotNull(e);
+	}
+	
+	/**
+	 * Test doView() is able to set saved user id as an attribute
+	 * 
+	 * @throws PortletException
+	 * @throws IOException
+	 */
+	@Test
+	public void testDoView() throws PortletException, IOException {
+		
+		String userId = "123";
+		
+		RenderRequestGenerator requestGenerator = portletTester.getRenderRequestGenerator();
+		RenderResponseGenerator responseGenerator = portletTester.getRenderResponseGenerator();
+		
+		requestGenerator.setParameter(SamplePortlet.PARAM_USER_ID, userId);
+		
+		RenderRequest request = requestGenerator.generateRequest();
+		RenderResponse response = responseGenerator.generateResponse();
+		
+		portlet.doView(request, response);
+		
+		assertEquals(userId, request.getAttribute(SamplePortlet.REQUEST_ATTR_SAVED_USER_ID));
+	}
+	
+	
+	/**
+	 * Test processActionSaveUserId() is able to save user id to preferences and 
+	 * save it as render parameter
+	 * 
+	 * @throws IOException
+	 * @throws PortletException
+	 */
+	@Test
+	public void testProcessActionSaveUserId() throws IOException, PortletException {
+		
+		String userId = "123";
+		
+		ActionRequestGenerator requestGenerator = portletTester.getActionRequestGenerator();
+		ActionResponseGenerator responseGenerator = portletTester.getActionResponseGenerator();
+		
+		requestGenerator.setParameter(SamplePortlet.PARAM_USER_ID, userId);
+		
+		ActionRequest request = requestGenerator.generateRequest();
+		ActionResponse response = responseGenerator.generateResponse();
+		
+		portlet.processActionSaveUserId(request, response);
+		
+		/* verify results */
+		PortletPreferences preferences = request.getPreferences();
+		assertEquals(userId, preferences.getValue(SamplePortlet.PREFS_PREFERRED_USER_ID, ""));
+		
+		Map<String, String[]> params = response.getRenderParameterMap();
+		assertEquals(userId, params.get(SamplePortlet.PARAM_USER_ID)[0]);
 	}
 	
 	@After
